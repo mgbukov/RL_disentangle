81085947e6ea01797e54868be7afe2fc61a790da
xeparc <xeparc@gmail.com>
2023-01-08 01:56:55 +0200

    Accepted WIP code changes in "beam_search.py".
    Beam search's Node class now keeps the path from root to
    current node insted of pointer to parent.
    Edits in test_beam_search.py due to "stochastic" parameter
    of the environment.

--- src/infrastructure/beam_search.py
+++ src/infrastructure/beam_search.py
@@ -2,18 +2,6 @@ import heapq
 import numpy as np
 
 
-
-class Node2:
-
-    def __init__(self, item, path=tuple(), cost=None):
-        self.item = item
-        self.path = tuple(path)
-        self.cost = cost
-
-    def __hash__(self):
-        return hash(self.item)
-
-
 class BeamSearch:
     """Beam search works by limiting the size of the fringe to a fixed size k, called the
     beam size. At every step the algorithm expands all of the nodes in the fringe, instead
@@ -28,26 +16,22 @@ class BeamSearch:
     class Node:
         """Lightweight, nonpublic class for storing a tree node."""
 
-        __slots__ = "item", "parent", "action", "cost"  # Streamline memory usage.
+        __slots__ =  ("item", "path", "cost")
 
-        def __init__(self, item, parent, action, cost):
+        def __init__(self, item, path=tuple(), cost=None):
             """ Initialize a Node instance.
 
             Args:
                 item (Any): The state to which this node corresponds.
-                parent (Node): The node in the tree that generated this node.
-                action (action): The action that was applied to the parent's state to
-                    generate this node's state.
+                path (tuple): The path from root to `self`. Path is tuple of actions.
                 cost (float): The cost associated with the state of this node.
             """
             self.item = item
-            self.parent = parent
-            self.action = action
+            self.path = tuple(path)
             self.cost = cost
 
-        def __hash__(self): return hash(self.item)
-
-
+        def __hash__(self):
+            return hash(self.item)
 
     #------------------------------ container initializer -------------------------------#
     def __init__(self, beam_size=1):
@@ -75,149 +59,56 @@ class BeamSearch:
             path (list[actions]): A list of actions giving the path from the start state
                 to the solution state.
         """
+        envstate = env.states
         goal = self._run(psi, env, qubit, num_iters, verbose)
         solution = goal.path if goal is not None else []
-        env.states = np.expand_dims(psi, 0)
+        env.states = envstate
         return solution
 
     #--------------------------------- private methods ----------------------------------#
-    # def _run(self, psi, env, qubit, num_iters, verbose):
-    #     """Start the tree search and continuously explore the state space until a solution
-    #     is found, or until the maximum number of iterations is reached. Return the goal
-    #     node if it is found, otherwise return None.
-    #     """
-    #     node = self.Node(item=psi, parent=None, action=None, cost=0)
-    #     fringe = [node]
-    #     for _ in range(num_iters):
-    #         # Batch the states from the fringe and set them as the environment states.
-    #         states = [node.item for node in fringe]
-    #         states = np.repeat(np.array(states), env.num_actions, axis=0)
-    #         env.states = states.copy()
-
-    #         # Transition into the next states by considering all possible actions.
-    #         actions = np.tile(np.array(list(env.actions.keys())), (len(fringe),))
-    #         next_states, _, _ = env.step(actions)
-
-    #         # Compute child costs and check if the goal is achieved.
-    #         costs = env.entropy().mean(axis=-1) if qubit is None else env.entropy()[:, qubit]
-    #         if (costs < env.epsi).any(): # Return the child node that reaches the goal.
-    #             for i, c in enumerate(costs):
-    #                 if c < env.epsi:
-    #                     assert env.Entropy(np.expand_dims(next_states[i], 0)) == c
-    #                     goal = self.Node(
-    #                         next_states[i],
-    #                         fringe[i // env.num_actions],
-    #                         actions[i],
-    #                         costs[i]
-    #                     )
-    #                     path = self._decode(goal)
-    #                     env.states = np.expand_dims(psi, 0)
-    #                     rollout_states = [env.states.copy()[0]]
-    #                     rollout_entropies = [env.entropy()[0,0]]
-    #                     print('\t roll:', env.entropy())
-    #                     for a in path:
-    #                         nxts, _, _ = env.step([a])
-    #                         print('\t roll:', env.entropy())
-    #                         rollout_states.append(nxts[0])
-    #                         rollout_entropies.append(env.entropy()[0,0])
-                        
-    #                     rollout_states = np.array(rollout_states)
-    #                     parent_states = [goal.item]
-    #                     parent_costs = [goal.cost]
-    #                     p = goal.parent
-        #                 while p is not None:
-        #                     parent_states.append(p.item)
-        #                     parent_costs.append(p.cost)
-        #                     p = p.parent
-        #                 parent_states.reverse()
-        #                 parent_costs.reverse()
-                        
-        #                 parent_states = np.array(parent_states)
-        #                 print(parent_costs, rollout_entropies)
-                        
-        #                 for s1, s2 in zip(rollout_states, parent_states):
-        #                     print(np.isclose(s1, s2).all())
-        #                 assert env.entropy()[0,0] <= c
-        #                 return goal
-
-        #     # Get the k-best successors and store them as the new fringe.
-        #     # successors = []
-        #     # for i, s in enumerate(next_states):
-        #     #     parent = fringe[i // env.num_actions]
-        #     #     assert np.all(parent.item == states[i])
-        #     #     action = actions[i]
-        #     #     cost = costs[i]
-        #     #     env.states = np.expand_dims(parent.item.copy(), 0)
-        #     #     nexts, _, _ = env.step([action])
-        #     #     assert np.all(nexts[0] ==  s)
-        #     #     assert env.entropy()[0,0] == cost
-        #     #     successors.append(self.Node(s, parent, action, cost))
-        #     successors = [self.Node(s, fringe[i // env.num_actions], actions[i], costs[i])
-        #                 for i,s in enumerate(next_states)]
-        #     fringe = self._getKBest(successors, self.beam_size)
-
-        #     # Maybe plot progress results.
-        #     if verbose:
-        #         beam_entropies = np.array([node.cost for node in fringe])
-        #         print("Mean entropies in current beam: ", beam_entropies.round(10).ravel())
-
-        # # If no solution is found for the given number of iterations, then return None.
-        # return None
-
     def _run(self, psi, env, qubit, num_iters, verbose=False):
-        fringe = [Node2(psi)]
+        """Start the tree search and continuously explore the state space
+        until a solution is found, or until the maximum number of iterations
+        is reached. Return the goal node if it is found, otherwise return None.
+        """
+        fringe = [BeamSearch.Node(psi)]
         states = [psi]
         for _ in range(num_iters):
+            # Batch the states from the fringe and set them as the
+            # environment states
             states = [node.item for node in fringe]
             states = np.repeat(np.array(states), env.num_actions, axis=0)
             assert len(states) == len(fringe) * env.num_actions
             env.states = states
-            # Transition into the next states by considering all possible actions.
+            # Transition into the next states by considering all possible actions
             actions = np.tile(np.array(list(env.actions.keys())), (len(fringe),))
             next_states, _, _ = env.step(actions)
+            # Compute child costs
             if qubit is None:
                 costs = env.entropy().mean(axis=-1)
             else:
                 costs = env.Entropy(next_states)[:, qubit]
             imincost = np.argmin(costs)
             mincost = costs[imincost]
+            # Check if a goal state is reached
             if mincost < env.epsi:
                 # assert env.Entropy(np.expand_dims(next_states[i], 0)) < env.epsi
                 path = fringe[imincost // env.num_actions].path
                 act = actions[imincost]
-                return Node2(next_states[imincost], path + (act,), mincost)
+                return BeamSearch.Node(next_states[imincost], path + (act,), mincost)
+            # Get the k-best successors and store them as the new fringe
             successors = []
             for i in range(len(next_states)):
                 parent = fringe[i // env.num_actions]
-                node = Node2(next_states[i], parent.path + (actions[i],), costs[i])
+                node = BeamSearch.Node(next_states[i], parent.path + (actions[i],), costs[i])
                 successors.append(node)
             fringe = self._getKBest(successors, self.beam_size)
             # Maybe plot progress results
             if verbose:
                 mean_cost = np.mean([node.cost for node in fringe])
                 print("Mean cost in current beam: ", mean_cost)
+        # If no solution is found for the given number of iterations return None
         return None
-
-    def _decode(self, goalNode):
-        """Given a goalNode produced from the tree search algorithm, trace the backward
-        path from the goal state to the start state by following the pointers to the
-        parents. Return the reversed path.
-
-        Args:
-            goalNode (Node): Node object returned from the tree search algorithm.
-
-        Returns:
-            path (list[action]): A list of actions giving the path from the start state to
-                the goalNode state.
-        """
-        if goalNode is None: return []
-        path = []
-        current = goalNode
-        while current.action is not None:
-            path.append(current.action)
-            current = current.parent
-        path.reverse()
-        return path
 
     @staticmethod
     def _getKBest(nodes, k):
--- tests/test_beam_search.py
+++ tests/test_beam_search.py
@@ -1,10 +1,8 @@
 """
 python3 test_beam_search.py -n 5 --beam_size 100 -t 10 --verbose
 """
-
 import argparse
 import sys
-import time
 
 import numpy as np
 
@@ -18,63 +16,6 @@ from src.infrastructure.beam_search import BeamSearch
 from src.infrastructure.logging import logBarchart
 from src.infrastructure.util_funcs import fix_random_seeds
 
-# Parse command line arguments.
-parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--seed", dest="seed", type=int, help="random seed value", default=0)
-parser.add_argument("-n", "--num_qubits", dest="num_qubits", type=int,
-                    help="Number of qubits in the quantum system", default=2)
-parser.add_argument("--epsi", dest="epsi", type=float,
-                    help="Threshold for disentanglement", default=1e-3)
-parser.add_argument("--beam_size", dest="beam_size", type=int,
-                    help="Size of the beam for beam search", default=10)
-parser.add_argument("-t", "--num_test", dest="num_test", type=int,
-                    help="Number of iterations to run the test for", default=1)
-parser.add_argument("--verbose", dest="verbose", action='store_true', default=False)
-args = parser.parse_args()
-
-
-# Global constants
-EPSI = args.epsi
-SEED = args.seed
-L = args.num_qubits
-
-
-np.set_printoptions(formatter={'all':lambda x: str(x)})
-num_test = args.num_test
-
-env = QubitsEnvironment(L, args.epsi, batch_size=1)
-beam_search = BeamSearch(beam_size=args.beam_size)
-
-tic = time.time()
-# # Test single-qubit disentanglement.
-# fix_random_seeds(args.seed)
-# entropies = np.ndarray((num_test, L, L))
-# path_lens = np.ndarray((num_test, L))
-# for i in range(num_test):
-#     env.set_random_states()
-#     psi = env.states
-
-#     for q0 in range(L):
-#         path = beam_search.start(np.squeeze(psi,axis=0), env, qubit=q0,
-#                                  num_iters=100, verbose=False)
-#         env.states = psi
-#         if args.verbose:
-#             print("====================================================")
-#             print(f"Trying to disentangle a single qubit: qubit {q0}")
-#             print(f"found a path with length {len(path)}: {path}")
-#             print(" entropy at step 0:", env.Entropy(env.states))
-#         for idx, a in enumerate(path):
-#             _ = env.step(a)
-#             if args.verbose:
-#                 print(f" entropy at step {idx+1}: {env.Entropy(env.states)}")
-#         entropies[i][q0] = env.Entropy(env.states)[0]
-#         path_lens[i][q0] = len(path)
-
-# for i in range(L):
-#     figname = f"/home/cacao-macao/Projects/RL_disentangle/logs/5qubits/single_qubit_disent_q{i}"
-#     figtitle = f"Disentangling qubit {i}\nAverage entropy after {path_lens.mean(axis=0)[i]:.1f} steps"
-#     logBarchart(figname, np.arange(L), entropies.mean(axis=0)[i], figtitle,
-#         labels={},ylim=(0, 0.7))
 
 def test_disent_first_qubit():
     _test_disent_first_qubit(4, 100, 10)
@@ -86,13 +27,13 @@ def test_disent_first_qubit():
 def _test_disent_first_qubit(nqubits=None, beam_size=100, repeats=10):
     """ Tests if Beam Search disentangles the first (leftmost) qubit."""
     beam_search = BeamSearch(beam_size)
-    for env in environment_generator(nqubits, 1, SEED, EPSI):
+    for env in environment_generator(nqubits, 1, False, SEED, EPSI):
         for _ in range(repeats):
-            print(env.L)
             env.set_random_states()
             psi = env.states.copy()
             solution = beam_search.start(env.states[0], env, 0, verbose=False)
             assert solution
+            print(env.L, 'qubits -> ', len(solution), 'steps')
             env.states = psi
             for a in solution:
                 env.step(a)
@@ -103,7 +44,7 @@ def test_disent_single_qubit(nqubits=4, beam_size=100, repeats=10):
     """ Tests if Beam Search disentangles i-th qubit if the (i - 1)-th are
     already disentangled. """
     fix_random_seeds(SEED)
-    env = QubitsEnvironment(nqubits, epsi=EPSI, batch_size=1)
+    env = QubitsEnvironment(nqubits, epsi=EPSI, batch_size=1, stochastic=False)
     beam_search = BeamSearch(beam_size)
     for _ in range(repeats):
         for i in range(nqubits - 1):
@@ -128,7 +69,7 @@ def test_disent_all_qubits():
     _test_disent_all_qubits(7, repeats=10)
 
 def _test_disent_all_qubits(nqubits=None, beam_size=100, repeats=10):
-    envgen = environment_generator(nqubits, 1, SEED, EPSI)
+    envgen = environment_generator(nqubits, 1, False, SEED, EPSI)
     for env in envgen:
         expert = SearchExpert(env)
         for _ in range(repeats):
@@ -162,11 +103,28 @@ def _test_disent_all_qubits(nqubits=None, beam_size=100, repeats=10):
 
 
 if __name__ == '__main__':
+    np.set_printoptions(formatter={'all':lambda x: str(x)})
+    # Parse command line arguments.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--seed", dest="seed", type=int,
+                        help="random seed value", default=0)
+    parser.add_argument("-n", "--num_qubits", dest="num_qubits", type=int,
+                        help="Number of qubits in the quantum system", default=2)
+    parser.add_argument("--epsi", dest="epsi", type=float,
+                        help="Threshold for disentanglement", default=1e-3)
+    parser.add_argument("--beam_size", dest="beam_size", type=int,
+                        help="Size of the beam for beam search", default=10)
+    parser.add_argument("-t", "--num_test", dest="num_test", type=int,
+                        help="Number of iterations to run the test for", default=1)
+    parser.add_argument("--verbose", dest="verbose", action='store_true', default=False)
+    args = parser.parse_args()
+
+    # Global constants
+    EPSI = args.epsi
+    SEED = args.seed
+
     test_disent_first_qubit()
     test_disent_single_qubit(args.num_qubits, repeats=args.num_test)
     test_disent_all_qubits()
-
-toc = time.time()
-print(f"{num_test} iters took {toc-tic:.3f} seconds")
 
 #
