f10acd41b2ea9ca9a7da12a2f4f8a5a7f117d84b
mgbukov <mgbukov@gmail.com>
2023-05-21 14:54:46 +0200

    adding extra env variables to be run code on mac; fixing plots to show action taken; adding more states and fixing state labeling convention

--- inference.py
+++ inference.py
@@ -18,8 +18,10 @@ python3 inference.py \
 """
 
 import argparse
-import os
+import sys,os
 
+import matplotlib
+matplotlib.use('Qt5Agg') # backend
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
@@ -27,6 +29,11 @@ from tqdm import tqdm
 
 from src.quantum_env import QuantumEnv
 from src.quantum_state import random_quantum_state
+
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True' # uncomment this line if omp error occurs on OSX for python 3
+os.environ['OMP_NUM_THREADS']='4' # set number of OpenMP threads to run in parallel
+os.environ['MKL_NUM_THREADS']='1' # set number of MKL threads to run in parallel
 
 
 parser = argparse.ArgumentParser()
@@ -51,48 +58,58 @@ bell = np.array([1/np.sqrt(2), 0, 0, -1/np.sqrt(2)]).astype(np.complex64)
 w = np.array([   0, 1/np.sqrt(3), 1/np.sqrt(3),   0, 1/np.sqrt(3),   0,   0,   0]).astype(np.complex64)
 zero = np.array([1, 0]).astype(np.complex64)
 one = np.array([0, 1]).astype(np.complex64)
-def rnd_state(q): np.random.seed(0); return random_quantum_state(q=q, prob=1.)
+def rnd_state(q,seed=0): np.random.seed(seed); return random_quantum_state(q=q, prob=1.)
 
 special_states = {
     # 4-qubit special states
     4: {
-        "|BB-0-0>": np.kron(bell, np.kron(zero, zero)),
-        "|0-BB-0>": np.kron(zero, np.kron(bell, zero)),
-        "|0-0-BB>": np.kron(zero, np.kron(zero, bell)),
-        "|BB-R-R>": np.kron(bell, np.kron(rnd_state(q=1), rnd_state(q=1))),
-        "|WWW-0>" : np.kron(w, zero),
-        "|WWW-R>" : np.kron(w, rnd_state(q=1)),
-        "|RR-R-R>": np.kron(rnd_state(q=2), np.kron(rnd_state(q=1), rnd_state(q=1))),
-        "|RR-RR>" : np.kron(rnd_state(q=2), rnd_state(q=2)),
-        "|RRR-0>" : np.kron(rnd_state(q=3), zero),
-        "|RRR-R>" : np.kron(rnd_state(q=3), rnd_state(q=1)),
-        "|RRRR>"  : rnd_state(q=4),
+        "|0>|0>|0>|0>": np.kron(zero, np.kron(zero, np.kron(zero, zero))),
+        "|BB>|0>|0>"  : np.kron(bell, np.kron(zero, zero)),
+        "|0>|BB>|0>"  : np.kron(zero, np.kron(bell, zero)),
+        "|0>|0>|BB>"  : np.kron(zero, np.kron(zero, bell)),
+        "|BB>|R>|R>"  : np.kron(bell, np.kron(rnd_state(q=1), rnd_state(q=1))),
+        "|WWW>|0>"    : np.kron(w, zero),
+        "|WWW>|R>"    : np.kron(w, rnd_state(q=1)),
+        "|RR>|R>|R>"  : np.kron(rnd_state(q=2), np.kron(rnd_state(q=1), rnd_state(q=1))),
+        "|RR>|RR>"    : np.kron(rnd_state(q=2), rnd_state(q=2)),
+        "|RRR>|0>"    : np.kron(rnd_state(q=3), zero),
+        "|RRR>|R>"    : np.kron(rnd_state(q=3), rnd_state(q=1)),
+        "|RRRR>"      : rnd_state(q=4),
+        "|RRRR>_1"    : rnd_state(q=4,seed=1),
+        "|RRRR>_2"    : rnd_state(q=4,seed=2),
+        "|RRRR>_3"    : rnd_state(q=4,seed=3),
+        "|RRRR>_4"    : rnd_state(q=4,seed=4),
     },
 
     # 5-qubit special states
     5: {
-        "|BB-0-0-0>": np.kron(bell, np.kron(zero, np.kron(zero, zero))),
-        "|0-BB-0-0>": np.kron(zero, np.kron(bell, np.kron(zero, zero))),
-        "|0-0-BB-0>": np.kron(zero, np.kron(zero, np.kron(bell, zero))),
-        "|0-0-0-BB>": np.kron(zero, np.kron(zero, np.kron(zero, bell))),
-        "|BB-R-R-R>": np.kron(bell, np.kron(rnd_state(q=1), np.kron(rnd_state(q=1), rnd_state(q=1)))),
-        "|BB-BB-0>" : np.kron(bell, np.kron(bell, zero)),
-        "|WWW-0-0>" : np.kron(w, np.kron(zero, zero)),
-        "|WWW-BB>"  : np.kron(w, bell),
-        "|RR-0-0-0>": np.kron(rnd_state(q=2), np.kron(zero, np.kron(zero, zero))),
-        "|RR-R-R-R>": np.kron(rnd_state(q=2), np.kron(rnd_state(q=1), np.kron(rnd_state(q=1), rnd_state(q=1)))),
-        "|RR-RR-0>" : np.kron(rnd_state(q=2), np.kron(rnd_state(q=2), zero)),
-        "|RR-RR-R>" : np.kron(rnd_state(q=2), np.kron(rnd_state(q=2), rnd_state(q=1))),
-        "|RRR-0-0>" : np.kron(rnd_state(q=3), np.kron(zero, zero)),
-        "|RRR-R-R>" : np.kron(rnd_state(q=3), np.kron(rnd_state(q=1), rnd_state(q=1))),
-        "|RRR-RR>"  : np.kron(rnd_state(q=3), rnd_state(q=2)),
-        "|RRRR-0>"  : np.kron(rnd_state(q=4), zero),
-        "|RRRR-R>"  : np.kron(rnd_state(q=4), rnd_state(q=1)),
-        "|RRRRR>"   : rnd_state(q=5),
+        "|0>|0>|0>|0>|0>": np.kron(zero,np.kron(zero, np.kron(zero, np.kron(zero, zero)))),
+        "|BB>|0>|0>|0>": np.kron(bell, np.kron(zero, np.kron(zero, zero))),
+        "|0>|BB>|0>|0>": np.kron(zero, np.kron(bell, np.kron(zero, zero))),
+        "|0>|0>|BB>|0>": np.kron(zero, np.kron(zero, np.kron(bell, zero))),
+        "|0>|0>|0>|BB>": np.kron(zero, np.kron(zero, np.kron(zero, bell))),
+        "|BB>|R>|R>|R>": np.kron(bell, np.kron(rnd_state(q=1), np.kron(rnd_state(q=1), rnd_state(q=1)))),
+        "|BB>|BB>|0>"  : np.kron(bell, np.kron(bell, zero)),
+        "|WWW>|0>|0>"  : np.kron(w, np.kron(zero, zero)),
+        "|WWW>|BB>"    : np.kron(w, bell),
+        "|RR>|0>|0>|0>": np.kron(rnd_state(q=2), np.kron(zero, np.kron(zero, zero))),
+        "|RR>|R>|R>|R>": np.kron(rnd_state(q=2), np.kron(rnd_state(q=1), np.kron(rnd_state(q=1), rnd_state(q=1)))),
+        "|RR>|RR>|0>"  : np.kron(rnd_state(q=2), np.kron(rnd_state(q=2), zero)),
+        "|RR>|RR>|R>"  : np.kron(rnd_state(q=2), np.kron(rnd_state(q=2), rnd_state(q=1))),
+        "|RRR>|0>|0>"  : np.kron(rnd_state(q=3), np.kron(zero, zero)),
+        "|RRR>|R>|R>"  : np.kron(rnd_state(q=3), np.kron(rnd_state(q=1), rnd_state(q=1))),
+        "|RRR>|RR>"    : np.kron(rnd_state(q=3), rnd_state(q=2)),
+        "|RRRR>|0>"    : np.kron(rnd_state(q=4), zero),
+        "|RRRR>|R>"    : np.kron(rnd_state(q=4), rnd_state(q=1)),
+        "|RRRRR>"      : rnd_state(q=5),
+        "|RRRRR>_2"    : rnd_state(q=5,seed=1),
+        "|RRRRR>_3"    : rnd_state(q=5,seed=2),
+        "|RRRRR>_4"    : rnd_state(q=5,seed=3),
+        "|RRRRR>_5"    : rnd_state(q=5,seed=4),
 }}
 
 
-def plot_model_output(figname, seq, attn, probs, acts):
+def plot_model_output(figname, seq, attn, probs, acts, act_taken):
     assert attn.shape[-2] == attn.shape[-1], "attention matrix must be square"
     assert len(seq) == attn.shape[-1], "attention matrix must match sequence length"
 
@@ -115,7 +132,8 @@ def plot_model_output(figname, seq, attn, probs, acts):
     ax = plt.subplot2grid(shape=(n_layers+1, n_heads), loc=(n_layers, 0), colspan=n_heads, fig=fig)
     xs = np.arange(len(probs))
 
-    pps = ax.bar(xs, probs)
+    pps = ax.bar(xs, probs, alpha=0.5)
+    pps[act_taken].set_alpha(1.0) # modify opacity of action taken
     for p in pps:
         height = p.get_height()
         ax.annotate("{:.3f}".format(height),
@@ -128,13 +146,18 @@ def plot_model_output(figname, seq, attn, probs, acts):
     ax.set_ylim(-0.05, 1.05)
     ax.tick_params(axis="both", labelsize=14)
 
+    #plt.show()
+
     fig.savefig(figname)
     plt.close(fig)
 
 
 # Load the model for file.
 model_fld = args.model_fld
-agent = torch.load(os.path.join(model_fld, "agent.pt"))
+if torch.cuda.is_available():
+    agent = torch.load(os.path.join(model_fld, "agent.pt"),)
+else:
+    agent = torch.load(os.path.join(model_fld, "agent.pt"), map_location=torch.device('cpu'))
 agent.policy_network.to(device)
 agent.value_network.to(device)
 for enc in agent.policy_network.net:
@@ -183,14 +206,15 @@ for sname, state in tqdm(special_states[num_qubits].items()):
         pi = agent.policy(o)
         probs = pi.probs[0].cpu().numpy()
 
-        # Make the plot.
-        seq = [f"q{i}q{j}" for i, j in env.simulator.actions.values()]
-        plot_model_output(os.path.join(log_dir, f"step_{s}.png"), seq, attn_weights, probs, env.simulator.actions)
-
         # Step the environment.
         acts = torch.argmax(pi.logits, dim=1)
         acts = acts.cpu().numpy()
         o, r, t, tr, i = env.step(acts)
+
+        # Make the plot.
+        seq = [f"q{i}q{j}" for i, j in env.simulator.actions.values()]
+        plot_model_output(os.path.join(log_dir, f"step_{s}.png"), seq, attn_weights, probs, env.simulator.actions, acts[0])
+
 
         path.append(acts[0])
         done = (t | tr)[0]
