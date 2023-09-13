96cf2045b9d5d638d77065c335c5abb5e6a507b9
xeparc <xeparc@gmail.com>
2023-09-13 12:58:57 +0300

    Comments in example script

--- tests/qiskit_isolated_example.py
+++ tests/qiskit_isolated_example.py
@@ -6,7 +6,6 @@ import sys
 dirname = os.path.dirname(os.path.realpath(__file__))
 project_root = os.path.realpath(os.path.join(dirname, os.path.pardir))
 sys.path.append(project_root)
-
 from qiskit.helpers import *
 from tests.test_qiskit_helpers import *
 
@@ -14,9 +13,19 @@ from tests.test_qiskit_helpers import *
 np.set_printoptions(precision=6, suppress=True)
 
 # Load diverging states
-diverging_states = np.load("diverging-states.npy")
+diverging_states = np.load(
+    os.path.join(os.path.dirname(__file__), "diverging-states.npy"))
 
-# Dictionary objects
+# Dictionary objects, keys = ['states', 'actions', 'entanglements',
+#                             'Us', 'RDMs', 'preswaps', 'postswaps']
+# IMPORTANT
+# The Us between qiskit and RL env rollouts may have columns 1,2 swapped,
+# if qiskit's U gate is premultiplied with swap gate on the right.
 qiskit_rollout = do_qiskit_rollout(diverging_states[-1], "ordered")
 rlenv_rollout = do_rlenv_rollout(diverging_states[-1], "ordered")
 
+print("\nFidelity between qiskit and RLenv rolllouts:")
+i = 0
+for x, y in zip(qiskit_rollout['states'], rlenv_rollout['states']):
+    print(f"states {i}:", fidelity(x, y))
+    i +=1
