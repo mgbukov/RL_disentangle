21e1eeb3d04a2773843167cfc5b2eabdda34d43f
xeparc <xeparc@gmail.com>
2025-08-26 17:14:11 +0300

    Test `rdm_2q_complex` observation function on CUDA.
    On RTX 4070Ti Super the speedup is 2X

--- scripts/gpu_observations.py
+++ scripts/gpu_observations.py
@@ -20,7 +20,7 @@ def gpu_rdm_2q_complex(states: np.ndarray):
     statevector = torch.from_numpy(states).to(device=GPU_DEVICE)
     statevector.requires_grad_ = False
 
-    rdms = torch.zeros((Q*(Q-1), 4, 4),
+    rdms = torch.zeros((Q*(Q-1), N, 4, 4),
                        dtype=torch.complex64,
                        requires_grad=False,
                        device=GPU_DEVICE)
@@ -29,9 +29,9 @@ def gpu_rdm_2q_complex(states: np.ndarray):
         sysA = tuple(q+1 for q in qubits)
         sysB = tuple(q+1 for q in range(Q) if q not in qubits)
         permutation = (0,) + sysA + sysB
-        psi = torch.permute(statevector, permutation).view(N, 4, -1)
+        psi = torch.permute(statevector, permutation).reshape(N, 4, -1)
         rdm = psi @ torch.permute(psi, (0, 2, 1)).conj()
-        rdms[i] = rdm
+        rdms[i, :, :, :] = rdm
 
     return torch.swapaxes(rdms, 0, 1).reshape(N, Q*(Q-1), 16)
 
@@ -43,11 +43,14 @@ if __name__ == "__main__":
     for B in (64, 128, 256):
         states = np.array([sample_haar_full(16) for _ in range(B)])
         tic = time.time()
-        obs = rdm_2q_complex(states)
+        cpu_obs = rdm_2q_complex(states)
         toc = time.time()
-        print(f"Elapsed time on CPU for batch size {B}: {toc-tic:.3f}")
+        print(f"Elapsed time on CPU for batch size {B}: {toc-tic:.3f}", flush=True)
 
         tic = time.time()
-        obs = gpu_rdm_2q_complex(states)
+        gpu_obs = gpu_rdm_2q_complex(states)
+        torch.cuda.synchronize()
         toc = time.time()
-        print(f"Elapsed time on GPU for batch size {B}: {toc-tic:.3f}")
+        print(f"Elapsed time on GPU for batch size {B}: {toc-tic:.3f}", flush=True)
+
+        assert np.allclose(cpu_obs, gpu_obs.cpu().numpy(), atol=1e-6)
